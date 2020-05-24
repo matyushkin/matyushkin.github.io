3b14dc0dcc90b026f5edf96ae0edc3eb3c00cf9e
matyushkin <leva.matyushkin@gmail.com>
2020-05-24 23:09:40 +0300

    Добавлена минимизация страниц с помощью eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,21 @@
-module.exports = {
-  dir: {
-    output: "../"
-  }
+const htmlmin = require("html-minifier");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  return {
+  	dir: {
+    	output: "../"
+  	}
+  };
 };
