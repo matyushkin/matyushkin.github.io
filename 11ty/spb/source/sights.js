bc74153d92011df30f1860e9db192c82d98def57
matyushkin <leva.matyushkin@gmail.com>
2020-05-28 21:01:03 +0300

    Добавлено оформление карточек категорий

--- 11ty/spb/source/sights.js
+++ 11ty/spb/source/sights.js
@@ -6,6 +6,8 @@ add_person_btn.onclick = function() {
 	let persons_cards = document.querySelector('.persons');
 	let person_card = document.querySelector('.person');
 	let new_person_card = document.createElement("div");
+	new_person_card.classList.add("person");
+	new_person_card.classList.add("card");
 	new_person_card.innerHTML = person_card.innerHTML;
 	persons_cards.appendChild(new_person_card);
 };
